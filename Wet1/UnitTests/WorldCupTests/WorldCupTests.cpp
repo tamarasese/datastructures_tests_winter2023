15fc90c91dcc0aa22da307551bd432cf0d237812
tamarasese <103326261+tamarasese@users.noreply.github.com>
2022-12-12 03:54:27 +0200

    Update WorldCupTests.cpp

--- Wet1/UnitTests/WorldCupTests/WorldCupTests.cpp
+++ Wet1/UnitTests/WorldCupTests/WorldCupTests.cpp
@@ -43,7 +43,7 @@ TEST_CASE("Insert and remove team")
         REQUIRE(res == StatusType::SUCCESS);
         delete obj;
     }
-/*
+
     SECTION("insert team with negative id")
     {
         world_cup_t *obj = new world_cup_t();
@@ -51,7 +51,7 @@ TEST_CASE("Insert and remove team")
         REQUIRE(res == StatusType::INVALID_INPUT);
         delete obj;
     }
-
+/*
     SECTION("insert team with negative points")
     {
         world_cup_t *obj = new world_cup_t();
